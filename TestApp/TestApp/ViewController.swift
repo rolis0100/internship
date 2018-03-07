7237294640bacd7c299d062df0477e828445865b
Roman <romanbondariev@romans-macbook-pro.local>
2018-03-07 15:04:57 +0200

    Create project

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Роман Бондарев on 07.03.2018.
+//  Copyright © 2018 Roman Bondariev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
